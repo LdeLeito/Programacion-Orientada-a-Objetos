59ce7a3202ff2eaeb3e0a66e8b3bbb13bdc98823
Oleos <leonardo.rua@outlook.es>
2026-09-25 17:23:47 -0300

    Implementar lógica de movimiento para la pieza Reina que combina el movimiento de la torre y del alfil:, permitiendo movimientos diagonales y capturas en el tablero.

--- tp02/src/main/java/trabajo/Reina.java
+++ tp02/src/main/java/trabajo/Reina.java
@@ -24,8 +24,34 @@ public class Reina extends Pieza {
         }
 
         Casilla origen = tablero.getCasilla(getX(), getY());
-        
 
+        int[][] direcciones = {
+                { -1, -1 },
+                { -1, 1 },
+                { 1, -1 },
+                { 1, 1 }
+        };
+
+        for (int[] direccion : direcciones) {
+            int nuevoX = getX() + direccion[0];
+            int nuevoY = getY() + direccion[1];
+            while (tablero.estaDentroDelTablero(nuevoX, nuevoY)) {
+                Casilla destino = tablero.getCasilla(nuevoX, nuevoY);
+                if (destino.estaVacia()) {
+                    movimientos.add(crearMovimiento(origen, destino));
+                } else {
+                    // Si es rival, puede capturarla.
+                    if (puedeOcupar(destino)) {
+                        movimientos.add(
+                                crearMovimiento(origen, destino));
+                    }
+                    // Cualquier pieza bloquea la diagonal.
+                    break;
+                }
+                nuevoX += direccion[0];
+                nuevoY += direccion[1];
+            }
         }
+        return movimientos;
     }
 }
