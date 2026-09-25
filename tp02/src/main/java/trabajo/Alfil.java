aa0510d372d374cc7faa1a59456cbbb18ebdd83a
Oleos <leonardo.rua@outlook.es>
2026-09-25 17:17:22 -0300

    Implementar lógica de movimiento para la pieza Alfil que es lo mismo que la torre pero en diagonal, permitiendo movimientos diagonales y capturas.

--- tp02/src/main/java/trabajo/Alfil.java
+++ tp02/src/main/java/trabajo/Alfil.java
@@ -24,8 +24,35 @@ public class Alfil extends Pieza {
         }
 
         Casilla origen = tablero.getCasilla(getX(), getY());
-        
 
+        int[][] direcciones = {
+                { -1, -1 },
+                { -1, 1 },
+                { 1, -1 },
+                { 1, 1 }
+        };
+
+        for (int[] direccion : direcciones) {
+            int nuevoX = getX() + direccion[0];
+            int nuevoY = getY() + direccion[1];
+
+            while (tablero.estaDentroDelTablero(nuevoX, nuevoY)) {
+                Casilla destino = tablero.getCasilla(nuevoX, nuevoY);
+
+                if (destino.estaVacia()) {
+                    movimientos.add(crearMovimiento(origen, destino));
+                } else {
+                    // si el rival la puede capturar
+                    if (puedeOcupar(destino)) {
+                        movimientos.add(crearMovimiento(origen, destino));
+                    }
+                    // Cualquier pieza que este en diagonal del movimiento la bloquea
+                    break;
+                }
+                nuevoX += direccion[0];
+                nuevoY += direccion[1];
+            }
         }
+        return movimientos;
     }
 }
